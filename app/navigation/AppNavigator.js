5f900492355dd2a4055f92eb7722ea097b1caef9
jaeha <jaeha23@naver.com>
2021-10-18 15:17:41 +0900

    AppNavigator

---
+++ app/navigation/AppNavigator.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+import Screen from '../components/Screen';
+import DayScreen from '../screens/DayScreen';
+import MonthScreen from '../screens/MonthScreen';
+import YearScreen from '../screens/YearScreen';
+
+const TopTab = createMaterialTopTabNavigator();
+
+const AppNavigator = () => {
+  return (
+    <Screen>
+      <TopTab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: '#f76707',
+          tabBarLabelStyle: {fontSize: 16},
+          tabBarIndicatorStyle: {backgroundColor: '#f76707'},
+          swipeEnabled: false,
+        }}>
+        <TopTab.Screen name="Day" component={DayScreen} />
+        <TopTab.Screen name="Month" component={MonthScreen} />
+        <TopTab.Screen name="Year" component={YearScreen} />
+      </TopTab.Navigator>
+    </Screen>
+  );
+};
+
+export default AppNavigator;
