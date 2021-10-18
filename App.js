121db7ddc3a6a8d1b05096401e0b844e5c363a6f
jaeha <jaeha23@naver.com>
2021-10-18 15:17:50 +0900

    App

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import AppNavigator from './app/navigation/AppNavigator';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 };
 
