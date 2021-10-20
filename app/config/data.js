d40fa80d95bd72892ae7a568f2014d115ac12086
jaeha <jaeha23@naver.com>
2021-10-20 17:41:44 +0900

    testData

---
+++ app/config/data.js
@@ -0,0 +1,67 @@
+export const testDotsData = {
+  '2021-10-25': {
+    dots: [
+      {
+        text: '밥 먹기',
+        color: 'blue',
+        done: false,
+      },
+      {
+        text: '캘린더 만들기',
+        color: 'red',
+        done: false,
+      },
+      {
+        text: '아이스 아메리카노 마시기',
+        color: 'black',
+        done: false,
+      },
+    ],
+  },
+  '2021-10-26': {
+    dots: [
+      {
+        text: '밥 먹기',
+        color: 'blue',
+        done: false,
+      },
+      {
+        text: '캘린더 만들기',
+        color: 'red',
+        done: false,
+      },
+    ],
+  },
+};
+
+export const testData = {
+  '2021-10-25': [
+    {
+      text: '밥 먹기',
+      color: 'blue',
+      done: false,
+    },
+    {
+      text: '캘린더 만들기',
+      color: 'red',
+      done: false,
+    },
+    {
+      text: '아이스 아메리카노 마시기',
+      color: 'black',
+      done: false,
+    },
+  ],
+  '2021-10-26': [
+    {
+      text: '밥 먹기',
+      color: 'blue',
+      done: false,
+    },
+    {
+      text: '캘린더 만들기',
+      color: 'red',
+      done: false,
+    },
+  ],
+};
