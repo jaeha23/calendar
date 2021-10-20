0234fedbc00782235a349cfed1a684a8c2e9b3fe
jaeha <jaeha23@naver.com>
2021-10-20 17:42:47 +0900

    DayScreen

--- app/screens/DayScreen.js
+++ app/screens/DayScreen.js
@@ -1,57 +1,9 @@
 import React, {Component} from 'react';
 import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-// import DateTimePicker from '@react-native-community/datetimepicker';
-
-// @ts-expect-error
 import {Agenda} from 'react-native-calendars';
-import {LocaleConfig} from 'react-native-calendars';
+import 'moment/locale/ko';
 
-import date from '../api/date';
-
-LocaleConfig.locales['ko'] = {
-  monthNames: [
-    '1월',
-    '2월.',
-    '3월',
-    '4월',
-    '5월',
-    '6월',
-    '7월.',
-    '8월',
-    '9월',
-    '10월',
-    '11월',
-    '12월',
-  ],
-  monthNamesShort: [
-    '1월',
-    '2월.',
-    '3월',
-    '4월',
-    '5월',
-    '6월',
-    '7월.',
-    '8월',
-    '9월',
-    '10월',
-    '11월',
-    '12월',
-  ],
-  dayNames: [
-    '일요일',
-    '월요일',
-    '화요일',
-    '수요일',
-    '목요일',
-    '금요일',
-    '토요일',
-  ],
-  dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-  today: "Aujourd'hui",
-};
-LocaleConfig.defaultLocale = 'ko';
-
-const initialDate = date();
+import {testDotsData, testData} from '../config/data';
 
 export default class DayScreen extends Component {
   state = {
@@ -64,58 +16,12 @@ export default class DayScreen extends Component {
         <Agenda
           items={this.state.items}
           loadItemsForMonth={this.loadItems.bind(this)}
-          selected={initialDate}
           renderItem={this.renderItem.bind(this)}
           renderEmptyDate={this.renderEmptyDate.bind(this)}
           rowHasChanged={this.rowHasChanged.bind(this)}
-          hideKnob={true}
-          monthFormat={'yyyy MMM'}
+          showClosingKnob={true}
           markingType="multi-dot"
-          markedDates={{
-            '2021-10-25': {
-              dots: [
-                {
-                  id: 1,
-                  height: 105,
-                  text: '밥 먹기',
-                  color: 'blue',
-                  done: false,
-                },
-                {
-                  id: 2,
-                  height: 105,
-                  text: '캘린더 만들기',
-                  color: 'red',
-                  done: false,
-                },
-                {
-                  id: 3,
-                  height: 105,
-                  text: '아이스 아메리카노 마시기',
-                  color: 'black',
-                  done: false,
-                },
-              ],
-            },
-            '2021-10-26': {
-              dots: [
-                {
-                  id: 1,
-                  height: 105,
-                  text: '밥 먹기',
-                  color: 'blue',
-                  done: false,
-                },
-                {
-                  id: 2,
-                  height: 105,
-                  text: '캘린더 만들기',
-                  color: 'red',
-                  done: false,
-                },
-              ],
-            },
-          }}
+          markedDates={testDotsData}
         />
       </>
     );
@@ -136,22 +42,7 @@ export default class DayScreen extends Component {
       });
       const newItems = {
         ...oldItems,
-        ...{
-          '2021-10-20': [],
-          '2021-10-21': [
-            {id: 1, height: 105, name: 'Item for 2021-10-21 #0'},
-            {id: 2, height: 76, name: 'Item for 2021-10-21 #1', add: true},
-          ],
-          '2021-10-22': [],
-          '2021-10-23': [
-            {id: 1, height: 80, name: 'item 2 - any js object', add: true},
-          ],
-          '2021-10-24': [],
-          '2021-10-25': [
-            {id: 1, name: 'item 3 - any js object'},
-            {id: 2, name: 'any js object', add: true},
-          ],
-        },
+        ...testData,
       };
       this.setState({
         items: newItems,
@@ -162,9 +53,9 @@ export default class DayScreen extends Component {
   renderItem(item) {
     return (
       <TouchableOpacity
-        style={[styles.item, {height: item.height}]}
-        onPress={() => Alert.alert(item.name)}>
-        <Text>{item.name}</Text>
+        style={[styles.item, {height: 50}, {color: item.color}]}
+        onPress={() => Alert.alert(item.text)}>
+        <Text>{item.text}</Text>
       </TouchableOpacity>
     );
   }
